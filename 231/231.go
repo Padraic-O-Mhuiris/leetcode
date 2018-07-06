0bf57467a1d869b3e2b7cf007d3a5e953c305463
sirromdev <sirromdev@gmail.com>
2018-07-06 17:20:39 +0100

    231

--- 231/231.go
+++ 231/231.go
@@ -2,38 +2,20 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 )
 
 func isPowerOfTwo(n int) bool {
 
-	if n <= 0 {
-		return false
-	}
-	numstring := strconv.FormatInt(int64(n), 2)
-	powflag := false
-
-	for _, a := range numstring {
-		if powflag && a == '1' {
-			return false
-		}
-
-		if !powflag && a == '1' {
-			powflag = true
-		}
-	}
-
-	if !powflag {
-		return false
-	}
-
-	return true
+	return n > 0 && ((n & (n - 1)) == 0) //O(1)
 }
 
 func main() {
 
-	for i := 0; i < 100; i++ {
-		fmt.Println(i, isPowerOfTwo(i))
+	for i := 0; i < 100000000000; i++ {
+		if isPowerOfTwo(i) {
+			fmt.Println(i)
+		}
+
 	}
 
 }
